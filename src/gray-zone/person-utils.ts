24984729510953389e2ec0e57603b33aced93ab9
Julien Robberechts <robberechts.consulting@gmail.com>
2023-11-21 00:43:25 +0100

    ai: ask AI for purity

--- src/gray-zone/person-utils.ts
+++ src/gray-zone/person-utils.ts
@@ -19,3 +19,33 @@ export function hasAddress(person: Person) {
   if (!person.address.street) return false;
   return true;
 }
+
+// q: Is the function "isAdult" pure or impure?
+// a: It's pure.
+
+// q: Does the function "isAdult" returns identical results for identical arguments?
+// a: Yes.
+
+// q: Is the return value of the function "isAdult" is only determined by its input values
+// a: Yes.
+
+// q: Does the function"isAdult" have any side effects?
+// a: No
+
+// q: Does the function"isAdult" modify any state or variables outside of the function ?
+// a: No, it doesn't.
+
+// q: Is the function "hasAddress" pure or impure?
+// a: It's pure.
+
+// q: Does the function "hasAddress" returns identical results for identical arguments?
+// a: Yes.
+
+// q: Is the return value of the function "hasAddress" is only determined by its input values
+// a: Yes.
+
+// Github Copilot chat
+//-----------------------
+
+// Is the function "hasAddress" pure or impure?
+// Yes, given the implementation of the function "hasAddress", it will return identical results for identical arguments. This function checks if the person object has an address property and if that address has both town and street properties. It doesn't rely on any external state or cause side effects, so it should return the same result for the same person input.
--- src/impure/console-log.ts
+++ src/impure/console-log.ts
@@ -1,3 +1,6 @@
 export function hello(name: string) {
   console.log(`Hello, ${name}!`);
 }
+
+// Is the function "hello" pure or impure?
+// The function "hello" is impure. It has a side effect of printing a message to the console. This is not a pure function because it modifies external state and produces observable effects other than returning a value.
