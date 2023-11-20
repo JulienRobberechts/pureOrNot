3aa7f8b53e99663a77a455ed24261553ef8a880a
Julien Robberechts <robberechts.consulting@gmail.com>
2023-11-21 00:39:21 +0100

    ai: ask ai if the function is pure

--- src/pure/add.ts
+++ src/pure/add.ts
@@ -1,3 +1,66 @@
 export function add(a: number, b: number) {
   return a + b;
 }
+
+// Github Copilot (in the code file)
+//-----------------------
+
+// q: Is the function "add" pure or impure?
+// a: It's pure.
+
+// q: Does the function "add" returns identical results for identical arguments?
+// a: Yes.
+
+// q: Is the return value of the function "add" is only determined by its input values
+// a: Yes.
+
+// q: Does the function"add" have any side effects?
+// a: No
+
+// q: Does the function"add" modify any state or variables outside of the function ?
+// a: No, it doesn't.
+
+// q: Does the function"add" read any state or variables outside of the function ?
+// a: No, it does not.
+
+// Github Copilot chat
+//-----------------------
+
+// Is the function "add" pure or impure?
+// The function "add" is pure. A pure function is a function where the return value is only determined by its input values, without observable side effects. This is a key feature of functional programming. The "add" function takes two numbers as input and returns their sum. It does not modify any state or variables outside of the function, so it is considered pure.
+
+// Does the function "add" returns identical results for identical arguments?
+// Yes, the function "add" will return identical results for identical arguments. This is because it's a pure function that only depends on the input arguments. For example, if you call `add(2, 3)`, it will always return `5`.
+
+// q: Is the return value of the function "add" is only determined by its input values
+// Yes, the return value of the function "add" is only determined by its input values. It takes two numbers as parameters and returns their sum. There are no external variables or states influencing the output of this function, making it a pure function.
+
+// Does the function"add" have any side effects?
+// No, the function "add" does not have any side effects. It takes two numbers as input and returns their sum. It does not modify any external state or variables, which means it's a pure function.
+
+// Does the function"add" modify any state or variables outside of the function ?
+// No, the function "add" does not modify any state or variables outside of the function. It only uses its input parameters `a` and `b` to compute and return their sum. This makes it a pure function.
+
+// Does the function"add" read any state or variables outside of the function ?
+// No, the function "add" does not read any state or variables outside of the function. It only uses its input parameters `a` and `b` to compute and return their sum. This makes it a pure function.
+
+// Sourcery chat
+//-----------------------
+
+// Is the function "add" pure or impure?
+// The function "add" in the code you provided is pure. It takes two numbers as input and returns their sum without causing any side effects.
+
+// Does the function "add" returns identical results for identical arguments?
+// Yes, the function "add" returns identical results for identical arguments. Given the same values for `a` and `b`, the function will always return their sum, which is a deterministic behavior.
+
+// Is the return value of the function "add" is only determined by its input values
+// Yes, the return value of the function "add" is only determined by its input values. The function simply adds the values of `a` and `b` and returns the result. There are no external dependencies or hidden state that could affect the return value.
+
+// Does the function"add" have any side effects?
+// No, the function "add" does not have any side effects. It only performs a simple addition operation on the input values `a` and `b` and returns the result. It does not modify any external state or produce any observable effects other than returning the sum.
+
+// Does the function"add" modify any state or variables outside of the function ?
+// No, the function "add" does not modify any state or variables outside of the function. It only takes the input values `a` and `b`, performs the addition operation, and returns the result. It does not have any side effects or modify any external state.
+
+// Does the function"add" read any state or variables outside of the function ?
+// No, the function "add" does not read any state or variables outside of the function. It only takes the input values `a` and `b`, performs the addition operation, and returns the result. It does
